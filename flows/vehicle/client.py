e6a5a112fad46cedd3a4d45dfdb59ab486f66498
jonashamerski <jonas.hamerski@almeidajunior.com.br>
2025-11-21 20:55:05 -0300

    feat/VEHICLE: adicionar nova identidade

--- flows/vehicle/client.py
+++ flows/vehicle/client.py
@@ -145,7 +145,7 @@ class PlacaAPIClient:
         headers = {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36git'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36'
         }
 
         try:
